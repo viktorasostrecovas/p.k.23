adacda4307b3c234b1533187289c755b07844670
viktorasostrecovas <desult666777@gmail.com>
2023-06-06 09:57:12 +0300

    first commit

--- src/perdaryta2.java
+++ src/perdaryta2.java
@@ -1,9 +1,9 @@
-public class uzduotis2 {
+public class perdaryta2 {
     public static void main(String[] args) {
 
         int x = 2;
         int y = 1;
-        if (x / y == 2) {
+        if (x % y == 0) {
             System.out.println(x * y);
         }
         else {
---
+++ src/perdaryta3.java
@@ -0,0 +1,21 @@
+public class perdaryta3 {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 5;
+        int x1 = 14;
+        int y1 = 5;
+        if (x % y == 0 ) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+        if (x1 % y1 == 0) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
